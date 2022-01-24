e5ba6d7bb880c67bdea07513a0cf0fe6d9df9f61
Powarox219 <moriniere.robin1877@gmail.com>
2022-01-24 17:19:29 +0100

    Clean code

--- Visualisation/ApexCharts/TreeMapCharts.js
+++ Visualisation/ApexCharts/TreeMapCharts.js
@@ -1,64 +1,61 @@
 var options = {
-          series: [
-          {
+    series: [{
             name: 'Desktops',
-            data: [
-              {
-                x: 'ABC',
-                y: 10
-              },
-              {
-                x: 'DEF',
-                y: 60
-              },
-              {
-                x: 'XYZ',
-                y: 41
-              }
+            data: [{
+                    x: 'ABC',
+                    y: 10
+                },
+                {
+                    x: 'DEF',
+                    y: 60
+                },
+                {
+                    x: 'XYZ',
+                    y: 41
+                }
             ]
-          },
-          {
+        },
+        {
             name: 'Mobile',
-            data: [
-              {
-                x: 'ABCD',
-                y: 10
-              },
-              {
-                x: 'DEFG',
-                y: 20
-              },
-              {
-                x: 'WXYZ',
-                y: 51
-              },
-              {
-                x: 'PQR',
-                y: 30
-              },
-              {
-                x: 'MNO',
-                y: 20
-              },
-              {
-                x: 'CDE',
-                y: 30
-              }
+            data: [{
+                    x: 'ABCD',
+                    y: 10
+                },
+                {
+                    x: 'DEFG',
+                    y: 20
+                },
+                {
+                    x: 'WXYZ',
+                    y: 51
+                },
+                {
+                    x: 'PQR',
+                    y: 30
+                },
+                {
+                    x: 'MNO',
+                    y: 20
+                },
+                {
+                    x: 'CDE',
+                    y: 30
+                }
             ]
-          }
-        ],
-          legend: {
-          show: false
-        },
-        chart: {
-          height: 350,
-          type: 'treemap'
-        },
-        title: {
-          text: 'Multi-dimensional Treemap',
-          align: 'center'
         }
-        };
+    ],
+    legend: {
+        show: false
+    },
+    chart: {
+        height: 350,
+        type: 'treemap'
+    },
+    title: {
+        text: 'Multi-dimensional Treemap',
+        align: 'center'
+    }
+};
 
-        var chart = new ApexCharts(document.querySelector("#ApexBarTreeMap"), options);
-        chart.render();
+var chart = new ApexCharts(document.querySelector("#ApexBarTreeMap"), options);
+chart.render();
