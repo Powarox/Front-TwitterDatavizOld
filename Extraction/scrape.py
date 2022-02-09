4830d89e7c177fdabd2870dd508ce03573a5fc40
Mohamed Lamine Seck <21711412@etu.unicaen.fr>
2022-02-09 14:47:47 +0100

    emojiExtraction: Script pour extraire et compter le nombre d' emojis d'un fichier csv, Mise a jour de scrape.py pour lui passer en paramettre le user qu'on veut extraire ses tweets en JSon

--- Extraction/scrape.py
+++ Extraction/scrape.py
@@ -1,8 +1,11 @@
 from xmlrpc.client import _datetime
 import snscrape.modules.twitter as twitterScrape
 import json
+import sys
 
-scraper = twitterScrape.TwitterUserScraper("KMbappe")
+nom = sys.argv[1]
+#"KMbappe"
+scraper = twitterScrape.TwitterUserScraper(nom)
 tweets = []
 
 for i,tweet in enumerate(scraper.get_items()):
@@ -10,9 +13,11 @@ for i,tweet in enumerate(scraper.get_items()):
         break
     tweets.append({"id" : tweet.id, "Content": tweet.content , "Username" : tweet.user.username })
     
-f = open("MbappeTweets.json","w")
+
+f = open("{}.json".format(nom),"w")
 j = json.dumps(tweets)
 f.write(j)
 f.close
 #print(f"{i} content: {tweet.content}") pour afficher sur la console 
 #tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username]) pour afficher  DateTime, tweet id, text, and username from the tweet object.
+#grinning
