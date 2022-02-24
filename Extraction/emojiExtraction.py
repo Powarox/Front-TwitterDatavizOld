650b5f7daf57aefba066fb8cb262b33a7c1206db
Mohamed Lamine Seck <21711412@etu.unicaen.fr>
2022-02-24 15:42:26 +0100

    scrape.py emojiExtraction.py

--- Extraction/emojiExtraction.py
+++ Extraction/emojiExtraction.py
@@ -3,7 +3,7 @@ import regex
 import csv 
 from collections import Counter
 import pandas as pd
-
+import json
 def split_count(text):
     emoji_list = []
     data = regex.findall(r'\X', text)
@@ -16,55 +16,19 @@ def split_count(text):
 
 
 # Ouvrir le fichier csv
-with open('user-tweets.csv', 'r') as f:
+with open('Cristiano-tweets.csv', 'r') as f:
     # Créer un objet csv à partir du fichier
     obj = csv.reader(f)
     emoji_list= []
     df =pd.DataFrame(obj)
-    text = df[2]
+    text = df[1]
     emoji_list= [] 
     for t in text:
         emoji_list=emoji_list+split_count(t)
+    f = open("{}.json".format("listeEmojisCristiano"),"w")
+    j = json.dumps(Counter(emoji_list))
+    f.write(j)
+    f.close
     print(Counter(emoji_list))
 
 
-
-"""
-
-
-import emoji
-import regex
-import csv 
-from collections import Counter
-import pandas as pd
-import numpy as np
-def split_count(text):
-
-    emoji_list = []
-    data = regex.findall(r'\X', text)
-    for word in data:
-        if any(char in emoji.UNICODE_EMOJI['en'] for char in word):
-            emoji_list.append(word)
-    
-    return emoji_list
-
-
-# Ouvrir le fichier csv
-with open('user-tweets.csv', 'r') as f:
-    # Créer un objet csv à partir du fichier
-    obj = csv.reader(f)
-    emoji_list= []
-    for ligne in obj:
-        #print(ligne[2])
-        counter = split_count(ligne[2])
-        #print(' '.join(emoji for emoji in counter))
-        #print(Counter(counter)) Pour compter le nombre d'occurence de chaque emojis
-        #print(pd.Series(emoji).value_counts())
-        count = Counter(emoji for emoji in counter) # Pass the list to instantiate the Counter object
-        #print(count)
-        #print(sum(count.values()))
-        #print(pd.value_counts(np.array(count)))
-        for t in ligne:
-            emoji_list=emoji_list+split_count(t)
-        print(Counter(emoji_list))    
-"""
--- Extraction/scrape.py
+++ Extraction/scrape.py
@@ -1,8 +1,10 @@
-from xmlrpc.client import _datetime
+#from xmlrpc.client import _datetime
 import snscrape.modules.twitter as twitterScrape
 import json
 import sys
-
+from datetime import date, datetime
+from json import dumps
+import csv
 nom = sys.argv[1]
 #"KMbappe"
 scraper = twitterScrape.TwitterUserScraper(nom)
@@ -11,13 +13,10 @@ tweets = []
 for i,tweet in enumerate(scraper.get_items()):
     if i > 1000:
         break
-    tweets.append({"id" : tweet.id, "Content": tweet.content , "Username" : tweet.user.username })
-    
+    tweets.append({"id" : tweet.id, "Content": tweet.content , "Username" : tweet.user.username, "Like":tweet.likeCount,"Retweet" :tweet.retweetCount, "ReplyCount" : tweet.replyCount, "Date":str(tweet.date) })
 
+print(tweets)
 f = open("{}.json".format(nom),"w")
 j = json.dumps(tweets)
 f.write(j)
 f.close
-#print(f"{i} content: {tweet.content}") pour afficher sur la console 
-#tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username]) pour afficher  DateTime, tweet id, text, and username from the tweet object.
-#grinning
